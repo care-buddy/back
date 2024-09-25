320e5c9f864154dc523a8ec8095d688430360bae
chacha <yushin.pak@gmail.com>
2024-09-25 16:32:06 +0900

    remove: 유저별 게시글 조회 API 라우터 삭제

--- server/routes/postRouter.ts
+++ server/routes/postRouter.ts
@@ -1,20 +1,26 @@
-import { Router } from "express";
-import postController from "../controller/postController";
-import upload from "../utils/imageHandler";
+import { Router } from 'express';
+import postController from '../controller/postController';
+import upload from '../utils/imageHandler';
 
-const postRouter = Router()
-postRouter.post("/", postController.createPost);   // 글 생성
-// postRouter.get("/", postController.confirmAllPost);   // 유저의 글 전체 조회 -> 사용안함
-postRouter.get("/", postController.confirmAllPost);   // 전체 게시글 조회 
-postRouter.get("/:_id", postController.confirmPost);    // 글 하나 조회
-postRouter.put("/:_id", postController.updatePost); // 수정
-postRouter.put("/:_id/d", postController.deletePost); // 삭제
-
-postRouter.put("/:_id/like", postController.likeHandle)  // 좋아요
+const postRouter = Router();
+postRouter.post('/', postController.createPost); // 글 생성
+postRouter.get('/', postController.confirmAllPost); // 전체 게시글 조회
+postRouter.get('/:_id', postController.confirmPost); // 글 하나 조회
+postRouter.put('/:_id', postController.updatePost); // 수정
+postRouter.put('/:_id/d', postController.deletePost); // 삭제
+postRouter.put('/:_id/like', postController.likeHandle); // 좋아요
 
 // 프로필 사진 등록
-postRouter.post("/:_id/postImage", upload.single("postImage"), postController.putPostImage);
+postRouter.post(
+  '/:_id/postImage',
+  upload.single('postImage'),
+  postController.putPostImage,
+);
 // 프로필 사진 삭제
-postRouter.delete("/:_id/postImage", upload.single("postImage"), postController.deletePostImage);
+postRouter.delete(
+  '/:_id/postImage',
+  upload.single('postImage'),
+  postController.deletePostImage,
+);
 
-export default postRouter
+export default postRouter;
