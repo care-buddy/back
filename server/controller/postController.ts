d26385fcd417bbc533bb020f9f3a31b7b7fe3cfd
chacha <yushin.pak@gmail.com>
2024-09-26 20:08:12 +0900

    Feat: post auth 미들웨어 추가 & 글 조회 API 수정

--- server/controller/postController.ts
+++ server/controller/postController.ts
@@ -5,64 +5,83 @@ import mongoose from 'mongoose';
 class PostController {
   // 글 생성
   async createPost(req: Request, res: Response) {
-    const datas = req.body;
-    const post = await postService.createPost(datas);
-    res.status(201).json({ success: true, data: post });
+    try {
+      const userId = req.user?._id; // 인증된 사용자 정보 활용
+      const datas = req.body;
+
+      const post = await postService.createPost({ userId, ...datas });
+      res.status(201).json({ success: true, data: post._id });
+    } catch (err: any) {
+      res.status(500).json({ err: err.message });
+    }
   }
 
   // 전체 게시글 조회
   async confirmAllPost(req: Request, res: Response) {
     try {
       const posts = await postService.confirmAllPosts();
-
-      res.status(200).json({ success: true, message: posts });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ success: false, message: '게시글 조회 중 오류 발생', error });
+      res.status(200).json({ success: true, data: posts });
+    } catch (err: any) {
+      res.status(500).json({ success: false, message: '게시글 조회 중 오류 발생', error: err.message });
     }
   }
-  // 글 하나 조회
+
+  // 글 하나 조회 
   async confirmPost(req: Request, res: Response) {
-    const { _id } = req.params;
-    const objectId = new mongoose.Types.ObjectId(_id);
-    const post = await postService.confirmUserPosts(objectId);
-    res.status(200).json({ success: true, message: post });
+    try {
+      const { _id } = req.params;
+      const objectId = new mongoose.Types.ObjectId(_id);
+
+      const post = await postService.confirmUserPosts(objectId);
+      res.status(200).json({ success: true, data: post });
+    } catch (err: any) {
+      res.status(500).json({ err: err.message });
+    }
   }
 
   // 글 수정
   async updatePost(req: Request, res: Response) {
-    const { _id } = req.params;
-    const postData = req.body;
-    const objectId = new mongoose.Types.ObjectId(_id);
-    const updatePost = await postService.updatePost(objectId, postData);
-    res
-      .status(200)
-      .json({ success: true, message: '성공적으로 수정이 완료되었습니다' });
+    try {
+      const { _id } = req.params;
+      const postData = req.body;
+      const objectId = new mongoose.Types.ObjectId(_id);
+
+      const updatePost = await postService.updatePost(objectId, postData);
+      res.status(200).json({ success: true, message: '성공적으로 수정이 완료되었습니다', data: updatePost });
+    } catch (err: any) {
+      res.status(500).json({ err: err.message });
+    }
   }
 
   // 글 삭제
   async deletePost(req: Request, res: Response) {
-    const { _id } = req.params;
-    const objectId = new mongoose.Types.ObjectId(_id);
+    try {
+      const { _id } = req.params;
+      const objectId = new mongoose.Types.ObjectId(_id);
 
-    const deletePost = await postService.deletePost(objectId);
-    res
-      .status(200)
-      .json({
+      const deletePost = await postService.deletePost(objectId);
+      res.status(200).json({
         success: true,
         message: '글 삭제가 완료되었습니다',
         data: deletePost,
       });
+    } catch (err: any) {
+      res.status(500).json({ err: err.message });
+    }
   }
 
-  // 좋아요
+  // 좋아요 - 미들웨어 적용 필요
   async likeHandle(req: Request, res: Response) {
-    const { _id } = req.params;
-    const { userId } = req.body;
-    const objectId = new mongoose.Types.ObjectId(_id);
-    const like = await postService.likeChange(objectId, userId);
-    res.status(200).json({ success: true, message: like });
+    try {
+      const { _id } = req.params;
+      const { userId } = req.body;
+      const objectId = new mongoose.Types.ObjectId(_id);
+
+      const like = await postService.likeChange(objectId, userId);
+      res.status(200).json({ success: true, data: like });
+    } catch (err: any) {
+      res.status(500).json({ err: err.message });
+    }
   }
 
   // 프로필 사진 등록
@@ -71,36 +90,141 @@ class PostController {
       const { _id } = req.params;
       const postImage = req.file?.filename;
       const objectId = new mongoose.Types.ObjectId(_id);
+
       const result = await postService.updatePostImage(objectId, postImage);
-      console.log(result);
-      res
-        .status(200)
-        .json({ message: '이미지가 수정되었습니다.', data: result });
+      res.status(200).json({ message: '이미지가 수정되었습니다.', data: result });
     } catch (err: any) {
-      console.log(err);
-      res
-        .status(500)
-        .json({ message: '서버의 postContrller에서 에러가 났습니다.' });
+      res.status(500).json({ message: '서버의 postController에서 에러가 났습니다.', error: err.message });
     }
   }
 
-  // 프로필 사진 삭제
+  // 프로필 사진 삭제 - 미들웨어 적용 필요
   async deletePostImage(req: Request, res: Response) {
     try {
       const { _id } = req.params;
-      const postImage = `public/image/defaultImage.png`;
+      const postImage = 'public/image/defaultImage.png';
       const objectId = new mongoose.Types.ObjectId(_id);
+
       const result = await postService.updatePostImage(objectId, postImage);
-      res
-        .status(200)
-        .json({ message: '이미지가 삭제되었습니다.', data: result });
-    } catch (error) {
-      console.log(error);
-      res
-        .status(500)
-        .json({ message: '서버의 postContrller에서 에러가 났습니다.' });
+      res.status(200).json({ message: '이미지가 삭제되었습니다.', data: result });
+    } catch (err: any) {
+      res.status(500).json({ message: '서버의 postController에서 에러가 났습니다.', error: err.message });
     }
   }
 }
+
 const postController = new PostController();
 export default postController;
+
+
+// import postService from '../services/postServices';
+// import { Request, Response } from 'express';
+// import mongoose from 'mongoose';
+
+// class PostController {
+//   // 글 생성
+//   async createPost(req: Request, res: Response) {
+//     try {
+//       const userId = req.user?._id; // 인증된 사용자 정보 활용
+//       const datas = req.body;
+
+//       const post = await postService.createPost({ userId, ...datas });
+//       res.status(201).json({ success: true, data: post });
+//     } catch (err: any) {
+//       res.status(500).json({ err: err.message });
+//     }
+//   }
+
+//   // 전체 게시글 조회
+//   async confirmAllPost(req: Request, res: Response) {
+//     try {
+//       const posts = await postService.confirmAllPosts();
+
+//       res.status(200).json({ success: true, message: posts });
+//     } catch (error) {
+//       res
+//         .status(500)
+//         .json({ success: false, message: '게시글 조회 중 오류 발생', error });
+//     }
+//   }
+//   // 글 하나 조회 - 미들웨어 적용 필요
+//   async confirmPost(req: Request, res: Response) {
+//     const { _id } = req.params;
+//     const objectId = new mongoose.Types.ObjectId(_id);
+//     const post = await postService.confirmUserPosts(objectId);
+//     res.status(200).json({ success: true, message: post });
+//   }
+
+//   // 글 수정
+//   async updatePost(req: Request, res: Response) {
+//     const { _id } = req.params;
+//     const postData = req.body;
+//     const objectId = new mongoose.Types.ObjectId(_id);
+//     const updatePost = await postService.updatePost(objectId, postData);
+//     res
+//       .status(200)
+//       .json({ success: true, message: '성공적으로 수정이 완료되었습니다' });
+//   }
+
+//   // 글 삭제 - 미들웨어 적용 필요
+//   async deletePost(req: Request, res: Response) {
+//     const { _id } = req.params;
+//     const objectId = new mongoose.Types.ObjectId(_id);
+
+//     const deletePost = await postService.deletePost(objectId);
+//     res.status(200).json({
+//       success: true,
+//       message: '글 삭제가 완료되었습니다',
+//       data: deletePost,
+//     });
+//   }
+
+//   // 좋아요 - 미들웨어 적용 필요
+//   async likeHandle(req: Request, res: Response) {
+//     const { _id } = req.params;
+//     const { userId } = req.body;
+//     const objectId = new mongoose.Types.ObjectId(_id);
+//     const like = await postService.likeChange(objectId, userId);
+//     res.status(200).json({ success: true, message: like });
+//   }
+
+//   // 프로필 사진 등록
+//   async putPostImage(req: Request, res: Response) {
+//     try {
+//       const { _id } = req.params;
+//       const postImage = req.file?.filename;
+//       const objectId = new mongoose.Types.ObjectId(_id);
+//       const result = await postService.updatePostImage(objectId, postImage);
+//       console.log(result);
+//       res
+//         .status(200)
+//         .json({ message: '이미지가 수정되었습니다.', data: result });
+//     } catch (err: any) {
+
+//       console.log(err);
+//       res
+//         .status(500)
+//         .json({ message: '서버의 postContrller에서 에러가 났습니다.' });
+//     }
+//   }
+
+//   // 프로필 사진 삭제 - 미들웨어 적용 필요(지금 x)
+//   async deletePostImage(req: Request, res: Response) {
+//     try {
+//       const { _id } = req.params;
+//       const postImage = `public/image/defaultImage.png`;
+//       const objectId = new mongoose.Types.ObjectId(_id);
+//       const result = await postService.updatePostImage(objectId, postImage);
+//       res
+//         .status(200)
+//         .json({ message: '이미지가 삭제되었습니다.', data: result });
+//     } catch (error) {
+//       console.log(error);
+//       res
+//         .status(500)
+//         .json({ message: '서버의 postContrller에서 에러가 났습니다.' });
+//     }
+//   }
+// }
+// const postController = new PostController();
+// export default postController;
--- server/db/models/postModel.ts
+++ server/db/models/postModel.ts
@@ -1,84 +1,105 @@
-import mongoose from "mongoose";
-import { Post, checkPost } from "../schemas/post";
+import mongoose from 'mongoose';
+import { Post, checkPost } from '../schemas/post';
 
 export class PostModel {
   // 글 생성
   async createPost(postData: checkPost) {
-    const newPost = await Post.create(postData)
+    const newPost = await Post.create(postData);
     return newPost;
   }
 
   // 전체 글 조회
   async findAllPosts() {
     const posts = await Post.find({})
-    .populate('userId')
-    .populate('communityId')
-    .populate('commentId');
+      .populate('userId')
+      .populate('communityId')
+      .populate('commentId');
     return posts;
   }
+
+  // // 글 하나 조회
+  // async findById(_id: mongoose.Types.ObjectId) {
+  //   const posts = await Post.find({ _id })
+  //   .populate('userId')
+  //   .populate('communityId')
+  //   .populate('commentId');
+  //   return posts;
+  // } // 글에서 유저에 대한 정보와 어느 카테고리에 속한지를 보여주고 싶을 때?
 
   // 글 하나 조회
   async findById(_id: mongoose.Types.ObjectId) {
     const posts = await Post.find({ _id })
-    .populate('userId')
-    .populate('communityId')
-    .populate('commentId');
+      .populate('userId') 
+      .populate('communityId')
+      .populate({
+        path: 'commentId', 
+        populate: {
+          path: 'userId', 
+          select: 'nickName', 
+        },
+      });
     return posts;
-  } // 글에서 유저에 대한 정보와 어느 카테고리에 속한지를 보여주고 싶을 때?
+  }
 
   // 글 수정
   async updatePost(_id: mongoose.Types.ObjectId, postData: checkPost) {
     const post = await Post.findOneAndUpdate({ _id }, postData, { new: true })
-    .populate('userId')
-    .populate('communityId')
-    .populate('commentId');
+      .populate('userId')
+      .populate('communityId')
+      .populate('commentId');
     return post;
   }
 
   // 글 삭제
   async deletePost(_id: mongoose.Types.ObjectId) {
-    const deletepost = await Post.findOneAndUpdate({ _id }, {deletedAt: new Date()}, { new: true });
+    const deletepost = await Post.findOneAndUpdate(
+      { _id },
+      { deletedAt: new Date() },
+      { new: true },
+    );
     return deletepost;
   }
 
-  async likeChange(_id: mongoose.Types.ObjectId, userId: mongoose.Types.ObjectId) {
+  async likeChange(
+    _id: mongoose.Types.ObjectId,
+    userId: mongoose.Types.ObjectId,
+  ) {
     const post = await Post.findById(_id);
-    if (!post) return { status: 404, err: '작업에 필요한 게시글이 없습니다.' }
-  
+    if (!post) return { status: 404, err: '작업에 필요한 게시글이 없습니다.' };
+
     const alreadyLiked = post.likedUsers.includes(userId);
     let updatedPost;
-    
+
     if (alreadyLiked) {
       // 이미 좋아요를 누른 상태이므로 좋아요를 제거
       updatedPost = await Post.findByIdAndUpdate(
-        _id, 
-        { $pull: { likedUsers: userId } }, 
-        { new: true }  
+        _id,
+        { $pull: { likedUsers: userId } },
+        { new: true },
       );
     } else {
       // 아직 좋아요를 누르지 않은 상태이므로 좋아요를 추가
       updatedPost = await Post.findByIdAndUpdate(
-        _id, 
-        { $push: { likedUsers: userId } }, 
-        { new: true }  
+        _id,
+        { $push: { likedUsers: userId } },
+        { new: true },
       );
     }
-  
+
     // 최종적으로 변경된 likedUsers 배열의 길이를 반환
     const LikesArray = updatedPost?.likedUsers;
     return LikesArray;
   }
-  
 
   // 프로필 사진 등록
   async updatePostImage(_id: mongoose.Types.ObjectId, postImage?: string) {
     const result = await Post.findOneAndUpdate(
       { _id },
       { postImage },
-      { new: true }
+      { new: true },
     );
     return result;
   }
 }
-const postModel = new PostModel()
-export default postModel 
+const postModel = new PostModel();
+export default postModel;
